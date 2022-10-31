f95c93a7690df96de3bf56882212d5c8f9c1f2ee
juffyinaba <jufikk@gmail.com>
2022-10-31 14:21:25 +0700

    nextjs-02-02-file-based-routing

---
+++ pages/posts/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface PostListPageProps {
+}
+
+export default function PostListPage (props: PostListPageProps) {
+  return (
+    <div> Post List Page </div>
+  );
+}
