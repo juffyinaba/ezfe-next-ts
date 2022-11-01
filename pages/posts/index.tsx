0dbdb55d681a11f6e3069d0950eecd1490596fd7
juffyinaba <jufikk@gmail.com>
2022-11-01 15:40:35 +0700

    nextjs-02-06-ssg posts-index

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'; 
 
+
 export interface PostListPageProps {
+  posts: any[]
 }
 
-export default function PostListPage (props: PostListPageProps) {
-  
-
+export default function PostListPage () {
   return (
     <div>
       <h1>This is a place to get all post</h1>
-      <p>It&apos;s still building</p>
     </div>
   );
 }
