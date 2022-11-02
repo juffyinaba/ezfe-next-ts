cf2355bac4766e23195317b25509a9e7f7b02d33
juffyinaba <jufikk@gmail.com>
2022-11-02 09:24:31 +0700

    nextjs-02-07-ssg-getstaticpaths [postsId]

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,13 +1,48 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { GetStaticProps, GetStaticPropsContext, GetStaticPaths } from 'next';
 
-export interface PostDetailPageProps {}
+export interface PostDetailPageProps {
+    posts: any[]
+}
 
-export default function PostDetailPage(props: PostDetailPageProps) {
+export default function PostDetailPage({ posts }: PostDetailPageProps) {
     const router = useRouter();
 
     return <div>
         <h1>Post Detail Page</h1>
         <p>Query: {JSON.stringify(router.query)}</p>
+        <code>
+            {JSON.stringify(posts)}
+        </code>
     </div>
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const response = await fetch('https://reqres.in/api/posts?per_page=12')
+    const data = await response.json()
+    
+    return {
+        paths: data.data.map((post: any) => ({ params: { postId: String(post.id) } })),
+        fallback: false,
+    }
+}
+
+export const getStaticProps: GetStaticProps<PostDetailPageProps> = async (
+    context: GetStaticPropsContext
+) => {
+    // server-side
+    // - only run at built-time, production (yarn start)
+    // - run each time code save, development (yarn dev)
+    console.log('called getStaticProps: ', context.params?.postId)
+    const postId = context.params?.postId
+    if (!postId) return { notFound: true }
+    const response = await fetch(`https://reqres.in/api/posts/${postId}`)
+    const data = await response.json()
+
+    return {
+        props: {
+            posts: data.data,
+        },
+    }
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react'; 
 import { GetStaticProps, GetStaticPropsContext } from 'next';
+import Link from 'next/link';
 
 export interface PostListPageProps {
   posts: any[]
 }
 
 export default function PostListPage ({posts}: PostListPageProps) {
-  console.log(posts)
+  // console.log(posts)
 
   return (
     <div>
       <h1>This is a place to get all post</h1>
       <ul>
-        {posts.map((post) => <li key={post}>{post}</li>)}
+        {/* {posts.map((post) => <li key={post.id}>{post.name}</li>)} */}
+        {posts.map((post) => <li key={post.id}><Link href={`/posts/${post.id}`}>{post.id}. {post.name} 🔗</Link></li>)}
       </ul>
     </div>
   );
@@ -23,9 +25,12 @@ export const getStaticProps: GetStaticProps<PostListPageProps> = async (context:
   // - only run at built-time, production (yarn start)
   // - run each time code save, development (yarn dev)
   console.log('called getStaticProps')
+  const response = await fetch('https://reqres.in/api/posts?per_page=12')
+  const data = await response.json()
+  // console.log(data.data)
   return {
     props: {
-      posts: ['Python', 'JavaScript', 'HTML', 'CSS', 'TypeScript'],
+      posts: data.data,
     },
   }
 }
